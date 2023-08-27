331b229c48f80fc6ce5a667226eee616bca92d7a
Ritesh Kumar <rk7212103@gmail.com>
2023-08-27 19:05:17 +0530

    adding disney changes

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCXThsIoAC9XL7J5xqRMWOAV6CCMA-UzYw",
+    authDomain: "disneyplus-clone-62a37.firebaseapp.com",
+    projectId: "disneyplus-clone-62a37",
+    storageBucket: "disneyplus-clone-62a37.appspot.com",
+    messagingSenderId: "1065595443230",
+    appId: "1:1065595443230:web:f09a095d0afa4a51f9760f",
+    measurementId: "G-KBZM25PG96"
+  };
+
+
+  const firebaseapp = fireba
